4e0e94342972a8469b3fe758343309dad8da3774
jmding0714 <jmding0714@gmail.com>
2018-08-13 22:47:07 -0700

    1. Add MaxPointsOnALine 2. Remove unnecessary using statements

---
+++ MaxPointsOnALine.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode_CSharp
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+        public Point() { x = 0; y = 0; }
+        public Point(int a, int b) { x = a; y = b; }
+    }
+
+    /// <summary>
+    /// Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
+    /// </summary>
+    public class MaxPointsOnALine
+    {
+        /// <summary>
+        /// Iterate through each pair of points. 
+        /// Calculate slop and store the number of points which has same slop in a dictionary
+        /// </summary>
+        /// <returns>The maximum number of points on a line</returns>
+        /// <param name="points">an array of points</param>
+        public int MaxPoints(Point[] points)
+        {
+            int globalMax = 0;
+            for (int i = 0; i < points.Length; i++)
+            {
+                Dictionary<decimal, int> dict = new Dictionary<decimal, int>();
+                int numOfSamePoints = 1;
+
+                for (int j = i + 1; j < points.Length; j++)
+                {
+                    // Consider the situations where points are the same (0/0), x axis of points are the same (/0)
+                    if (points[i].x == points[j].x && points[i].y == points[j].y)
+                    {
+                        numOfSamePoints++;
+                    }
+                    else if (points[i].x == points[j].x)
+                    {
+                        dict[decimal.MaxValue] = dict.ContainsKey(decimal.MaxValue) ? dict[decimal.MaxValue] + 1 : 1;
+                    }
+                    else
+                    {
+                        decimal slop = ((decimal)points[j].y - (decimal)points[i].y) / ((decimal)points[j].x - (decimal)points[i].x);
+                        dict[slop] = dict.ContainsKey(slop) ? dict[slop] + 1 : 1;
+                    }
+                }
+
+                // Iterate through the dictionary, find local max
+                int localMax = 0;
+                foreach (KeyValuePair<decimal, int> tuple in dict)
+                {
+                    localMax = tuple.Value > localMax ? tuple.Value : localMax;
+                }
+                // Update local max with number of same points
+                localMax += numOfSamePoints;
+                // Update global max
+                globalMax = localMax > globalMax ? localMax : globalMax;
+            }
+
+            return globalMax;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LeetCode_CSharp
 {
@@ -10,7 +6,8 @@ namespace LeetCode_CSharp
     {
         static void Main(string[] args)
         {
-            RunLargestRectangleInHistogram();
+            //RunLargestRectangleInHistogram();
+            RunMaxPointsOnALine();
         }
 
         #region LargestRectangleInHistogram
@@ -19,6 +16,19 @@ namespace LeetCode_CSharp
             LargestRectangleInHistogram test = new LargestRectangleInHistogram();
             int[] input = new int[] { 2, 1, 5, 6, 2, 3 };
             int output = test.LargestRectangleArea2(input);
+            Console.WriteLine(output);
+        }
+        #endregion
+
+        #region MaxPointsOnALine
+        static void RunMaxPointsOnALine()
+        {
+            MaxPointsOnALine test = new MaxPointsOnALine();
+            Point[] points = new Point[3];
+            points[0] = new Point(0, 0);
+            points[1] = new Point(94911151, 94911150);
+            points[2] = new Point(94911152, 94911151);
+            int output = test.MaxPoints(points);
             Console.WriteLine(output);
         }
         #endregion
