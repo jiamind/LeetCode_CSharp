f32d5e04a880972c446fc958d8a4d21e9c1f4307
jmding0714 <jmding0714@gmail.com>
2018-08-15 01:09:59 -0700

    Add EvaluateReversePolishNotation

---
+++ EvaluateReversePolishNotation.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode_CSharp
+{
+    /// <summary>
+    /// Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+    /// Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+    /// Note:
+    /// Division between two integers should truncate toward zero.
+    /// The given RPN expression is always valid. That means the expression would always evaluate to a result and there won't be any divide by zero operation.
+    /// </summary>
+    class EvaluateReversePolishNotation
+    {
+        // Defines valid operations
+        public static List<string> operators = new List<string>() { "+", "-", "*", "/" };
+
+        /// <summary>
+        /// Evaluates the value of an arithmetic expression in Reverse Polish Notation
+        /// </summary>
+        /// <returns>the value of the evaluation</returns>
+        /// <param name="tokens">array of tokens</param>
+        public int EvalRPN(string[] tokens)
+        {
+            // Create a stack to store numbers and evaluation results
+            Stack<int> stack = new Stack<int>();
+            foreach (string token in tokens)
+            {
+                // Push numbers to the stack
+                // If the token is a valid operator, pop the last two values, evaluate and push the result
+                if (!operators.Contains(token))
+                {
+                    stack.Push(int.Parse(token));
+                }
+                else
+                {
+                    int rightValue = stack.Pop();
+                    int leftValue = stack.Pop();
+                    stack.Push(Evaluate(leftValue, rightValue, token));
+                }
+            }
+
+            // The stack should always end up with one value, which is the result of the whole evaluation
+            return stack.Pop();
+        }
+
+        /// <summary>
+        /// Evaluate using the specified leftValue, rightValue and operation.
+        /// </summary>
+        /// <returns>The value of the evaluation.</returns>
+        /// <param name="leftValue">Left value.</param>
+        /// <param name="rightValue">Right value.</param>
+        /// <param name="op">Operator.</param>
+        private static int Evaluate(int leftValue, int rightValue, string op)
+        {
+            switch (op)
+            {
+                case "+":
+                    return leftValue + rightValue;
+                case "-":
+                    return leftValue - rightValue;
+                case "*":
+                    return leftValue * rightValue;
+                case "/":
+                    return leftValue / rightValue;
+            }
+
+            return 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,14 +7,15 @@ namespace LeetCode_CSharp
         static void Main(string[] args)
         {
             //RunLargestRectangleInHistogram();
-            RunMaxPointsOnALine();
+            //RunMaxPointsOnALine();
+            RunEvaluateReversePolishNotation();
         }
 
         #region LargestRectangleInHistogram
         static void RunLargestRectangleInHistogram()
         {
             LargestRectangleInHistogram test = new LargestRectangleInHistogram();
-            int[] input = new int[] { 2, 1, 5, 6, 2, 3 };
+            int[] input = { 2, 1, 5, 6, 2, 3 };
             int output = test.LargestRectangleArea2(input);
             Console.WriteLine(output);
         }
@@ -29,6 +30,16 @@ namespace LeetCode_CSharp
             points[1] = new Point(94911151, 94911150);
             points[2] = new Point(94911152, 94911151);
             int output = test.MaxPoints(points);
+            Console.WriteLine(output);
+        }
+        #endregion
+
+        #region EvaluateReversePolishNotation
+        static void RunEvaluateReversePolishNotation()
+        {
+            EvaluateReversePolishNotation test = new EvaluateReversePolishNotation();
+            string[] input = { "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+" };
+            int output = test.EvalRPN(input);
             Console.WriteLine(output);
         }
         #endregion
