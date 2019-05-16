53d4c07ae337f98de3a5ea28f601a87bf7701836
jmding0714 <jmding0714@gmail.com>
2019-05-15 22:37:15 -0700

    Count Complete Tree Nodes

---
+++ CountCompleteTreeNodes.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using CountCompleteTreeNodes;
+
+namespace CountCompleteTreeNodes
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x)
+        {
+            val = x;
+        }
+    }
+}
+
+namespace LeetCode_CSharp
+{
+    /// <summary>
+    /// 222. Count Complete Tree Nodes
+    /// Given a complete binary tree, count the number of nodes.
+    /// Note:
+    /// Definition of a complete binary tree from Wikipedia:
+    /// In a complete binary tree every level, except possibly the last, is completely filled, 
+    /// and all nodes in the last level are as far left as possible.
+    /// It can have between 1 and 2h nodes inclusive at the last level h.
+    /// </summary>
+    public class CountCompleteTreeNodes
+    {
+        public int CountNodes(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            int leftHeight = GetHeight(root.left);
+            int rightHeight = GetHeight(root.right);
+
+            // If the height of the left tree is equal to the height of the right tree
+            // It means that the last node is in the right tree
+            // Otherwise, it means the last node is in the left tree
+            if (leftHeight == rightHeight)
+            {
+                // Sum the nodes in the left tree and recursively call on the right tree
+                return (1 << leftHeight) + CountNodes(root.right);
+            }
+            else
+            {
+                // Sum the nodes in the right tree and recursively call on the left tree
+                return (1 << rightHeight) + CountNodes(root.left);
+            }
+        }
+
+        /// <summary>
+        /// Get the height of the tree by counting the height of the left tree
+        /// </summary>
+        /// <returns>The height.</returns>
+        /// <param name="root">Tree Root Node</param>
+        private int GetHeight(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            return 1 + GetHeight(root.left);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 namespace LeetCode_CSharp
 {
     using System.Linq;
+    using global::CountCompleteTreeNodes;
 
     public class Program
     {
@@ -40,7 +41,8 @@ namespace LeetCode_CSharp
             //RunOddEvenJump();
             //RunMostStonesRemovedWithSameRowOrColumn();
             //RunBackspaceStringCompare();
-            RunEvaluateDivision();
+            //RunEvaluateDivision();
+            RunCountCompleteTreeNodes();
 
             Console.ReadKey();
         }
@@ -381,12 +383,32 @@ namespace LeetCode_CSharp
         #endregion
 
         #region BackspaceStringCompare
-
         public static void RunBackspaceStringCompare()
         {
             BackspaceStringCompare test = new BackspaceStringCompare();
             string S = "ab#c##", T = "ad#c##";
             Console.WriteLine(test.BackspaceCompare(S, T));
+        }
+        #endregion
+
+        #region CountCompleteTreeNodes
+        public static void RunCountCompleteTreeNodes()
+        {
+            CountCompleteTreeNodes test = new CountCompleteTreeNodes();
+            TreeNode root = new TreeNode(1);
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            TreeNode node4 = new TreeNode(4);
+            TreeNode node5 = new TreeNode(5);
+            TreeNode node6 = new TreeNode(6);
+
+            root.left = node2;
+            root.right = node3;
+            node2.left = node4;
+            node2.right = node5;
+            node3.left = node6;
+
+            Console.WriteLine(test.CountNodes(root));
         }
         #endregion
 
